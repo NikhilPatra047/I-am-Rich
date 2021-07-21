654fa8663719d94c5b0f1205aede0b8b9f73e25a
NikhilPatra047 <patra.nikhil04@gmail.com>
2021-07-21 21:24:36 +0530

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Nikhil Kumar Patra on 21/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
